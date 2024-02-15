21f179ed725e1bde3369da77ad5b5d41a6011c00
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2024-02-15 11:44:16 +0100

    Daily back-up

--- EcuReal_vVcBsBpTpms_CLAR_LCI_I230_LATEST/b_git_push.sh
+++ EcuReal_vVcBsBpTpms_CLAR_LCI_I230_LATEST/b_git_push.sh
@@ -1,4 +1,4 @@
 git add .
 git status
-git commit -m "AUTOSAR Migration Activities - SwcApplTpms - EcuReal_vVcBsBpTpms_CLAR_LCI_I230_LATEST"
+git commit -m "AUTOSAR Migration Activities - SwcApplTpms - rEcu_vVcBsBpTpms_CLAR_LCI_I230_LATEST"
 git push
--- EcuResim_vVcBsBpTpms_cB_LATEST/b_git_push.sh
+++ EcuResim_vVcBsBpTpms_cB_LATEST/b_git_push.sh
@@ -1,4 +1,4 @@
 git add .
 git status
-git commit -m "AUTOSAR Migration Activities - SwcApplTpms - EcuResim_vVcBsBpTpms_cB_LATEST"
+git commit -m "AUTOSAR Migration Activities - SwcApplTpms - vEcu_vVcBsBpTpms_cB_LATEST"
 git push
